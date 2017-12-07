c13e6f3ab7f02b4703e52ebb548caa8d3d08bb68
Becky Roselius Haney <bhaney@calvin.edu>
2017-12-07 12:17:35 -0500

    All base matrices created and put into DF. Calc.IO function added.

---
+++ Calc_IO_metrics.R
@@ -0,0 +1,38 @@
+#
+# R script to calculate the IO Metrics 
+#
+
+##############################################################################################
+# Function: calc.IO.metrics
+# Calculates Information Matrix Metrics: H, X, Psi, TST, and alpha
+# for an input matrix.
+#
+# BRH: 2017.11.22
+#
+# Requires 1 argument: rxc matrix
+# Returns 1 vector: H,X,Psi,TST,alpha,F
+###########################################################################################################################################################################################
+
+calc.IO.metrics <-function(IO) {
+	TST=sum(IO)
+	N=nrow(IO)
+	R<-as.vector(rowSums(IO))
+	C<-as.vector(colSums(IO))
+	D=1/(R%*%t(C))
+	D[is.infinite(D)] <- 0
+		temp1 <- as.matrix(log(IO*(TST*D)))
+		temp1[is.infinite(temp1)] <- 0
+	X=sum(IO*temp1)
+		temp1 <- log((IO*IO)*D)
+		temp1[is.infinite(temp1)] <- 0
+	Psi= -sum(IO*(temp1))
+		temp1 <- log(IO/TST)
+		temp1[is.infinite(temp1)] <- 0
+	H <- -sum(IO*temp1)
+	alpha <- X/H
+	F	<- -exp(1)*alpha*log(alpha)
+	results.names <- c("TST", "H", "X", "Psi", "alpha", "F")
+	results <- matrix(cbind(TST,H,X,Psi,alpha,F),nrow=1) %>% 
+	  set_colnames(.,results.names)
+	results
+}
--- SimplePowerSector.R
+++ SimplePowerSector.R
@@ -9,7 +9,7 @@ library(matsindf) # For collapse_to_matrices and expand_to_tidy functions
 library(ggplot2)  # For awesome plotting functions
 
 rm(list=ls())
-
+source("Calc_IO_metrics.R")
 #
 # Initial Input Parameters:
 #
@@ -39,22 +39,88 @@ product.names   <- c(paste0("P", seq(1:Prod.n)))
 industry.names  <- c(paste0("I",seq(1:Ind.n)))
 fin.names       <- c(paste0("F",seq(1:Fin.n)))
 
-# Make the C matrix with the help of the byname package.
-# Do so in a way that is visually correct (by using byrow = TRUE), 
-# because it is simpler to debug.
-f.mat <- matrix(c(0,0,
-              0,0,
-              0,0,
-              0.6, 0.0,
-              0.4, 0.0, 
-              0.0, 1.0),
-            nrow = 6, ncol = 2, byrow = TRUE) %>%   
-            setrownames_byname(product.names) %>% # Products in rows
-            setrowtype("Products") %>% # Set row type to ensure errors are thrown when manipulating matrices with wrong types.
-            setcolnames_byname(fin.names) %>% # Industries in columns
-            setcoltype("Industries")  # Set col type to ensure errors are thrown when manipulating matrices with wrong types.
+f.split <- matrix(c(.5,.5),
+                  nrow = 1, ncol = 2, byrow = TRUE) %>%   
+  setrownames_byname("Products") %>% 
+  setrowtype("Products") %>% 
+  setcolnames_byname(fin.names) %>% 
+  setcoltype("Industries") 
 
- DF <- data.frame(F1 = seq(0, 1, by = 0.1)) %>% 
+f.product.coeffs <- matrix(
+            c(0,0,
+              0,0,
+              0.6,0,
+              0.4, 0.0,
+              0, 0.3, 
+              0.0, .7),
+            nrow = 6, ncol = 2, byrow = TRUE) %>%   
+            setrownames_byname(product.names) %>% 
+            setrowtype("Products") %>% 
+            setcolnames_byname(fin.names) %>% 
+            setcoltype("Industries")  
+
+Y.colsum <- elementproduct_byname(TFO, f.split)
+Y <- matrixproduct_byname(f.product.coeffs,hatize_byname(Y.colsum))
+y <- rowsums_byname(Y)
+
+A.mat <- matrix(c(0,0,1,1,0,0,
+                  0,0,0,0,1,1,
+                  0,0,0,0,0,0,
+                  0,0,0,0,0,0,
+                  0,0,0,0,0,0, 
+                  0,0,0,0,0,0),
+                nrow = 6, ncol = 6, byrow = TRUE) %>%   
+  setrownames_byname(product.names) %>% 
+  setrowtype("Products") %>% 
+  setcolnames_byname(industry.names) %>% 
+  setcoltype("Industries")  
+
+Mfg.etas <- matrix(c(1,1,1/3,0.40,0.40,0.50),nrow=1,ncol=Ind.n,byrow=T) %>%
+  setrownames_byname("Products") %>% 
+  setrowtype("Products") %>% 
+  setcolnames_byname(industry.names) %>% 
+  setcoltype("Industries") 
+
+Z <- elementquotient_byname(1,Mfg.etas)  %>% 
+     matrix(rbind(rep(.,6)),nrow=Prod.n,ncol=Ind.n) %>% 
+        setrownames_byname(product.names) %>% 
+        setrowtype("Products") %>% 
+        setcolnames_byname(industry.names) %>% 
+        setcoltype("Industries")  %>%
+        elementproduct_byname(.,A.mat) 
+
+D <- identize_byname(Z)
+
+A <- matrixproduct_byname(Z,transpose_byname(D))
+
+q <- matrixproduct_byname(invert_byname(Iminus_byname(A)),y)
+
+V <- matrixproduct_byname(transpose_byname(D),hatize_byname(q))
+
+g <- rowsums_byname(V)
+
+U <- matrixproduct_byname(Z,hatize_byname(g))
+
+IO <- cbind(U,Y)
+
+DF.base <- data.frame(scenario = 1) %>%
+  mutate( f.split = lapply(seq_len(nrow(.)), function(X) f.split),
+          f.product.coeffs = lapply(seq_len(nrow(.)), function(X) f.product.coeffs),
+          A.mat = lapply(seq_len(nrow(.)), function(X) A.mat),
+          Z = lapply(seq_len(nrow(.)), function(X) Z),
+          D = lapply(seq_len(nrow(.)), function(X) D),
+          A = lapply(seq_len(nrow(.)), function(X) A),
+          q = lapply(seq_len(nrow(.)), function(X) q),
+          V = lapply(seq_len(nrow(.)), function(X) V),
+          g = lapply(seq_len(nrow(.)), function(X) g),
+          U = lapply(seq_len(nrow(.)), function(X) U),
+          IO = lapply(seq_len(nrow(.)), function(X) IO) )
+
+
+  mutate(
+          ENA = lapply(seq_len(nrow(.)), function(X) calc.IO.metrics(IO)  )  )
+
+DF.scenario1 <- data.frame(F1 = seq(0, 1, by = 0.1)) %>% 
   mutate(
     F2 = 1 - F1,
     scenario.val = F1, # This becomes the metadata column
@@ -86,9 +152,8 @@ f.mat <- matrix(c(0,0,
     f.mat = lapply(seq_len(nrow(.)), function(X) f.mat), 
     ysum = elementproduct_byname(TFO, f),
     Y = matrixproduct_byname(f.mat,hatize_byname(ysum)),
-    # y = matrixproduct_byname(Y,as.vector(rep(1,Fin.n),mode="any"))
-    # rowsums_byname is easier :-)
     y = rowsums_byname(Y)
+    
   )
 
 
