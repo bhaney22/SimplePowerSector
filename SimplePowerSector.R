61ec16304a2c58532eb3f6e87547075720310898
Becky Roselius Haney <bhaney@calvin.edu>
2017-12-11 15:58:49 -0500

    Code to graph the network added. Not correct yet.

--- SimplePowerSector.R
+++ SimplePowerSector.R
@@ -7,6 +7,9 @@ library(tibble)   # For the rownames_to_column function.  (Not sure why this did
 library(lazyeval) # For the interp function.  (Not sure why this didn't come in with matsindf.)
 library(matsindf) # For collapse_to_matrices and expand_to_tidy functions
 library(ggplot2)  # For awesome plotting functions
+library(statnet)
+library(igraph)
+library(qgraph)
 
 rm(list=ls())
 source("Calc_IO_metrics.R")
@@ -281,7 +284,7 @@ DF.scenarios <- data.frame(rbind(DF.IO.1))
 # Once all scenarios are built, create all of the eurostat matrices 
 # and the IO flow tables that come from them.
 #
-# MKH Question: The following warning gest thrown for every observation
+# MKH Question: The following warning gets thrown for every observation
 # when building the eurostat DF. From my trying different things, I think
 # it has something to do with the Mfg.etas.mat * A.mat, but not sure.
 # 
@@ -310,26 +313,81 @@ DF.eurostat <- data.frame(DF.scenarios) %>%
 #
 # STEP 4: Run the results
 # BRH TODO: calculate PRR in currency units. 
-# MKH question: How to use sumall_byname with just two columns of a matrix?
 #################################################################################
 DF.results <- data.frame(DF.eurostat) %>% 
-  mutate(IO.phys.sumall = sumall_byname(IO.phys),
-         IO.curr.sumall = sumall_byname(IO.curr),
-         PRR.phys = TFO/(as.numeric(IO.phys.sumall)-TFO) )
-
- DF.results  <- DF.results %>% mutate(TST.phys = lapply(X=IO.phys, function(X) calc.TST(X)),
-                                      alpha.phys = lapply(X=IO.phys, function(X) calc.alpha(X)),
-                                      F.phys = lapply(X=IO.phys, function(X) calc.F(X)),
-                                      TST.curr = lapply(X=IO.curr, function(X) calc.TST(X)),
-                                      alpha.curr = lapply(X=IO.curr, function(X) calc.alpha(X)),
-                                      F.curr = lapply(X=IO.curr, function(X) calc.F(X)))
+  mutate(
+        IO.phys.sumall = sumall_byname(IO.phys),
+        IO.curr.sumall = sumall_byname(IO.curr),
+        PRR.phys = TFO/(as.numeric(IO.phys.sumall)-TFO),
+        TST.phys = lapply(X=IO.phys, function(X) calc.TST(X)),
+        alpha.phys = lapply(X=IO.phys, function(X) calc.alpha(X)),
+        F.phys = lapply(X=IO.phys, function(X) calc.F(X)),
+        TST.curr = lapply(X=IO.curr, function(X) calc.TST(X)),
+        alpha.curr = lapply(X=IO.curr, function(X) calc.alpha(X)),
+        F.curr = lapply(X=IO.curr, function(X) calc.F(X)))
 
 
  
 #########################################################################################################
-# End 
+# End  Results
 #########################################################################################################
 
+#########################################################################################################
+# Begin Plots
+#########################################################################################################
+
+
+ 
+ ##################################################################################
+ # Function: create.network.pdf
+ # Draw the network: help pages are here
+ # http://igraph.org/r/doc/graph_from_adjacency_matrix.html 
+ ##################################################################################
+# 
+# Set indexes to point to different nodes.
+#
+nodes.n	<- Res.n + Mfg.n + Fin.n
+Mfg.first	<- Res.n + 1		# node number of first Mfg
+Fin.first	<- Res.n + Mfg.n + 1	# node number of first Fin
+
+Res.nodes	<- seq(1,Res.n)
+Mfg.nodes	<- seq(Mfg.first,length=Mfg.n)
+Fin.nodes	<- seq(Fin.first,length=Fin.n)
+
+   groups 	<- list(	"Input Units" =c(Res.nodes),
+                    "Efficiency"=c(Mfg.nodes),
+                    "Output Units"=c(Fin.nodes))
+   groups.curr	<- list(	"Input Prices"=c(Res.nodes),
+                        "Value-Added"=c(Mfg.nodes),
+                        "Output Prices"=c(Fin.nodes))
+   # 
+   shapes <- c(rep("ellipse",Res.n),rep("rectangle",Mfg.n),rep("ellipse",Fin.n))
+   L <- matrix(c(
+     0,2.5, 0,.5,
+     1,3, 1,2, 1,1, 1,0,
+     2,2.5, 2,.5),
+     ncol=2,byrow=TRUE)
+   # 
+   # options(width=200)
+   # pdf(file=paste(image.dir,"Flows_phys_",run.num,".pdf",sep=""))
+   qgraph(DF.results$IO.phys[[5]][,3:8],  ### BRH TODO - figure this out
+          edge.labels=T,
+          edge.label.cex=1.25,edge.color="black",fade=F,
+          # BRH Commented out during csv file creation
+          # not working edge.label.position=c(rep(.5,Res.n),rep(.7,Mfg.n),rep(.7,Fin.n)),
+          layout=L,
+          groups=groups,
+          borders=F,
+          labels=c(industry.names,fin.names),
+          # nodeNames=nodes.names.phys.legend,
+          shape=shapes,
+          palette="colorblind",
+          title=paste("(Flows in MW)",sep=""))
+   # dev.off()
+   # 
+   # options(width=200)
+   # pdf(file=paste(image.dir,"Flows_curr_",run.num,".pdf",sep=""))
+  #  }
 
 
 
