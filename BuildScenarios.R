ea57b74e3f7fe965f06696750fa20e3d7cb47b55
MatthewHeun <matthew.heun@me.com>
2017-12-12 19:03:53 -0500

    f.product.coeffs is now working.

--- BuildScenarios.R
+++ BuildScenarios.R
@@ -54,36 +54,64 @@ f.split_list <- lapply(f1s, function(f1){
 running_list_for_expand.grid$f.split <- f.split_list
 
 # Work on f.product.coeffs
-f.product.coeffs_list <- lapply(fpcs, function(fpc){
-  list(
-    fpc31 = fpc, fpc32 = fpc,
-    fpc41 = fpc, fpc42 = fpc,
-    fpc51 = fpc, fpc52 = fpc
+f.product.coeffs_DF <- list(
+  fpc31 = fpcs, fpc32 = fpcs,
+  fpc41 = fpcs, fpc42 = fpcs,
+  fpc51 = fpcs, fpc52 = fpcs
+) %>% 
+  expand.grid() %>% 
+  mutate(
+    fpc61 = 1 - fpc31 - fpc41 - fpc51,
+    fpc62 = 1 - fpc32 - fpc42 - fpc52
   ) %>% 
-    expand.grid() %>% 
-    mutate(
-      fpc61 = 1 - fpc31 - fpc41 - fpc51,
-      fpc62 = 1 - fpc32 - fpc42 - fpc52
-    ) %>% 
-    filter(fpc61 >= 0 & fpc62 >= 0) %>% 
-    mutate(
-      fpc11 = 0, fpc12 = 0,
-      fpc21 = 0, fpc22 = 0
-    ) %>% 
-    gather(key = "var", value = "val", fpc11, fpc12,
-                                       fpc21, fpc22,
-                                       fpc31, fpc32,
-                                       fpc41, fpc42,
-                                       fpc51, fpc52,
-                                       fpc61, fpc62) %>% 
-    mutate(
-      
-    )
-    
-    
-})
-fpc.names <- paste0("fpc_", c("31", "32", "41", "42", "51", "52"))
-fpc_list <- lapply(fpc.names, function(fpcn){fpcs}) %>% set_names(fpc.names)
+  filter(fpc61 >= 0 & fpc62 >= 0) %>% 
+  mutate(
+    fpc11 = 0, fpc12 = 0,
+    fpc21 = 0, fpc22 = 0
+  ) %>% 
+  rownames_to_column("scenario") %>% 
+  mutate(
+    scenario = as.numeric(scenario)
+  ) %>% 
+  gather(key = "var", value = "val", fpc11, fpc12,
+         fpc21, fpc22,
+         fpc31, fpc32,
+         fpc41, fpc42,
+         fpc51, fpc52,
+         fpc61, fpc62) %>% 
+  mutate(
+    rownames = case_when(
+      startsWith(.data$var, "fpc1") ~ "P1",
+      startsWith(.data$var, "fpc2") ~ "P2",
+      startsWith(.data$var, "fpc3") ~ "P3",
+      startsWith(.data$var, "fpc4") ~ "P4",
+      startsWith(.data$var, "fpc5") ~ "P5",
+      startsWith(.data$var, "fpc6") ~ "P6"
+    ),
+    colnames = case_when(
+      endsWith(.data$var, "1") ~ "F1",
+      endsWith(.data$var, "2") ~ "F2",
+      TRUE ~ NA_character_
+    ),
+    rowtypes = "Products",
+    coltypes = "Sectors", 
+    matnames = "f.product.coeffs"
+  ) %>% 
+  group_by(scenario) %>% 
+  collapse_to_matrices(values = "val", matnames = "matnames", 
+                       rownames = "rownames", colnames = "colnames", 
+                       rowtypes = "rowtypes", coltypes = "coltypes") %>% 
+  rename(f.product.coeffs = val) %>%
+  mutate(scenario = NULL)
+
+running_list_for_expand.grid$f.product.coeffs <- f.product.coeffs_DF$f.product.coeffs
+
+# Work on gammas
+
+
+
+
+
 
 # gammas
 # gamma.names <- paste0("gamma_", names(mfg.etas.base))
@@ -104,18 +132,18 @@ fpc_list <- lapply(fpc.names, function(fpcn){fpcs}) %>% set_names(fpc.names)
 # }
 
 DF.scenario.factors <- expand.grid(running_list_for_expand.grid) #%>%
-  # mutate(
-  #   # Calculate auxiliary varlues
-  #   f2 = 1 - f1,
-  #   fpc_61 = 1 - fpc_31 - fpc_41 - fpc_51, 
-  #   fpc_62 = 1 - fpc_32 - fpc_42 - fpc_52
-  # ) %>% 
-  # # None of the scenarios with negative values for fpc_61 or fpc_62 are valid scenarios.
-  # filter(fpc_61 >= 0 & fpc_62 >= 0) %>% 
-  # # Promote the row names (which are simply integers) to a column to provide scenario identifiers
-  # rownames_to_column("scenario.n") %>% 
-  # # reorder columns
-  # select(scenario.n, tfo, f1, f2, starts_with("fpc"), everything()) 
+# mutate(
+#   # Calculate auxiliary varlues
+#   f2 = 1 - f1,
+#   fpc_61 = 1 - fpc_31 - fpc_41 - fpc_51, 
+#   fpc_62 = 1 - fpc_32 - fpc_42 - fpc_52
+# ) %>% 
+# # None of the scenarios with negative values for fpc_61 or fpc_62 are valid scenarios.
+# filter(fpc_61 >= 0 & fpc_62 >= 0) %>% 
+# # Promote the row names (which are simply integers) to a column to provide scenario identifiers
+# rownames_to_column("scenario.n") %>% 
+# # reorder columns
+# select(scenario.n, tfo, f1, f2, starts_with("fpc"), everything()) 
 
 # temp <- DF.scenario.factors %>% 
 #   select(scenario.n, f1, f2) %>% 
