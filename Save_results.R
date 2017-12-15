786883573591bbe1423abfa29f90d95461a8eabb
Becky Roselius Haney <bhaney@calvin.edu>
2017-12-14 22:36:48 -0500

    Saves results as DF.

--- Save_results.R
+++ Save_results.R
@@ -8,8 +8,6 @@
 # Future TODO: use mutate and lapply to build a DF that is written to the .csv file.
 #
 ##############################################################################################
-library(readr)
-
 rm(list=ls())
 load("DF.results")
 
@@ -55,94 +53,56 @@ curr.scale.display <- "Millions USD"
 #############################################################################################################
 ## Generate .csv file that has all of the factors and the results.
 #############################################################################################################
-RESULTS = paste("Row",
-                "Eta.1",
-                "Eta.2",
-                "Eta.3",
-                "Eta.4",
-                "VA.1",
-                "VA.2",
-                "VA.3",
-                "VA.4",
-                "Res.1.price",
-                "Res.2.price",
-                "Fin.1.price",
-                "Fin.2.price",
-                "Fin.1.Mkt.share",
-                "Fin.1.I.1.share",
-                "Fin.1.I.2.share",
-                "Fin.1.I.3.share",
-                "Fin.1.I.4.share",
-                "Fin.2.I.1.share",
-                "Fin.2.I.2.share",
-                "Fin.2.I.3.share",
-                "Fin.2.I.4.share",
-                "TFO","TST.phys","PRR.phys",
-                "GDP.curr","TST.curr","PRR.curr",
-                "alpha.phys","F.phys",
-                "alpha.curr","F.curr",
-                sep=",")
+DF.results.f <- DF.results %>%
+  mutate(Row=seq(1,nrow(DF.results)),
+                Eta.1=lapply(X=Mfg.etas.mat, function(X) X[1,3]),
+                Eta.2=lapply(X=Mfg.etas.mat, function(X) X[1,4]),
+                Eta.3=lapply(X=Mfg.etas.mat, function(X) X[1,5]),
+                Eta.4=lapply(X=Mfg.etas.mat, function(X) X[1,6]),
+                Res.1.price=lapply(X=Prices.mat, function(X) X[1,1]),
+                Res.2.price=lapply(X=Prices.mat, function(X) X[2,1]),
+                Fin.1.price=lapply(X=Prices.mat, function(X) X[Mfg.first,Fin.first]),
+                Fin.2.price=lapply(X=Prices.mat, function(X) X[Mfg.first,Fin.first+1]),
+                Fin.1.Mkt.share=lapply(X=f.split, function(X) X[1]),
+                Fin.1.I.1.share=lapply(X=f.product.coeffs, function(X) X[3,1]),
+                Fin.1.I.2.share=lapply(X=f.product.coeffs, function(X) X[4,1]),
+                Fin.1.I.3.share=lapply(X=f.product.coeffs, function(X) X[5,1]),
+                Fin.1.I.4.share=lapply(X=f.product.coeffs, function(X) X[6,1]),
+                Fin.2.I.1.share=lapply(X=f.product.coeffs, function(X) X[3,2]),
+                Fin.2.I.2.share=lapply(X=f.product.coeffs, function(X) X[4,2]),
+                Fin.2.I.3.share=lapply(X=f.product.coeffs, function(X) X[5,2]),
+                Fin.2.I.4.share=lapply(X=f.product.coeffs, function(X) X[6,2]),
+                Flows.phys=lapply(X=IO.phys, function(IO.phys) {
+                    matrix(0,nrow=nodes.n,ncol=nodes.n)  %>%
+                    setrownames_byname(c(product.names,fin.names)) %>%
+                    setrowtype("Products") %>%
+                    setcolnames_byname(c(industry.names,fin.names)) %>%
+                    setcoltype("Industries") %>%
+                    sum_byname(.,IO.phys) %>% 
+                    sort_rows_cols(roworder=(c(product.names,fin.names)),
+                                   colorder=(c(industry.names,fin.names)))
+                }),
+                 Flows.curr=lapply(X=IO.curr, function(IO.curr){ 
+                   matrix(0,nrow=nodes.n,ncol=nodes.n) %>%
+                   setrownames_byname(c(product.names,fin.names)) %>%
+                   setrowtype("Products") %>%
+                   setcolnames_byname(c(industry.names,fin.names)) %>%
+                   setcoltype("Industries") %>%
+                   sum_byname(.,IO.curr) %>% 
+                   sort_rows_cols(roworder=(c(product.names,fin.names)),
+                                  colorder=(c(industry.names,fin.names)))
+                }),
+                   VA.1=lapply(X=Flows.curr,function(Flows.curr)
+                     sum(Flows.curr[Mfg.nodes[1],Fin.nodes])/sum(Flows.curr[,Mfg.nodes[1]])),
+                   VA.2=lapply(X=Flows.curr,function(Flows.curr)
+                     sum(Flows.curr[Mfg.nodes[2],Fin.nodes])/sum(Flows.curr[,Mfg.nodes[2]])),
+                   VA.3=lapply(X=Flows.curr,function(Flows.curr)
+                     sum(Flows.curr[Mfg.nodes[3],Fin.nodes])/sum(Flows.curr[,Mfg.nodes[3]])),
+                   VA.4=lapply(X=Flows.curr,function(Flows.curr)
+                     sum(Flows.curr[Mfg.nodes[4],Fin.nodes])/sum(Flows.curr[,Mfg.nodes[4]])),
+                    Mfg.etas.mat=NULL,Prices.mat=NULL,f.split=NULL,f.product.coeffs=NULL)
 
-for(row.num in 1:nrow(DF.results)) { 
 
-Mfg.etas = DF.results$Mfg.etas.mat[[row.num]][1,]
-Res.prices	= DF.results$Prices.mat[[row.num]][1:2,1]	
-Fin.prices  = DF.results$Prices.mat[[row.num]][Mfg.first,Fin.nodes]
-
-Flows.curr		<- matrix(0,nrow=nodes.n,ncol=nodes.n) %>%
-  setrownames_byname(c(product.names,fin.names)) %>%
-  setrowtype("Products") %>%
-  setcolnames_byname(c(industry.names,fin.names)) %>%
-  setcoltype("Industries") %>%
-  sum_byname(.,DF.results$IO.curr[[row.num]]) %>% 
-  sort_rows_cols(roworder=(c(product.names,fin.names)),
-                 colorder=(c(industry.names,fin.names)))
-
-#
-# Compute value-added for each industry
-#
-va	<-rep(0,Mfg.n)
-for (i in 1:Mfg.n) {
-  va[i] <- round(sum(Flows.curr[Mfg.nodes[i],Fin.nodes])/sum(Flows.curr[,Mfg.nodes[i]]),2)
-}
-
-#########################################################################################################
-# Begin .csv file
-#########################################################################################################
-
-row.results <- paste(row.num,
-Mfg.etas[3],Mfg.etas[4],Mfg.etas[5],Mfg.etas[6],va[1],va[2],va[3],va[4],
-Res.prices[1],Res.prices[2],
-Fin.prices[1],Fin.prices[2],
-DF.results$f.split[[row.num]][1],
-DF.results$f.product.coeffs[[row.num]][3,1],
-DF.results$f.product.coeffs[[row.num]][4,1],
-DF.results$f.product.coeffs[[row.num]][5,1],
-DF.results$f.product.coeffs[[row.num]][6,1],
-DF.results$f.product.coeffs[[row.num]][3,2],
-DF.results$f.product.coeffs[[row.num]][4,2],
-DF.results$f.product.coeffs[[row.num]][5,2],
-DF.results$f.product.coeffs[[row.num]][6,2],
-DF.results$TFO[[row.num]],
-DF.results$TST.phys[[row.num]],
-DF.results$PRR.phys[[row.num]],
-DF.results$GDP.curr[[row.num]],
-DF.results$TST.curr[[row.num]],
-DF.results$PRR.curr[[row.num]], 
-DF.results$alpha.phys[[row.num]],
-DF.results$F.phys[[row.num]],
-DF.results$alpha.curr[[row.num]],
-DF.results$F.curr[[row.num]],
-sep=",")
-
-RESULTS	<- rbind(RESULTS,row.results)
-row.num <- row.num + 1
-}
-
-write(RESULTS,"Results.csv")
-DF.results.f <- data.frame(read.csv("Results.csv"))
-DF.IO.mats <- select(DF.results,IO.phys, IO.curr)
-DF.results.f <- cbind(DF.results.f,DF.IO.mats)
 save(DF.results.f,file="DF.results.f")
 rm(list=ls())
 
