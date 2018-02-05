e9d88063e83b437fe43e38b5a58879e41195d324
Becky Roselius Haney <bhaney@calvin.edu>
2018-02-04 22:07:41 -0500

    more descriptive titles and file names

--- Draw_network.R
+++ Draw_network.R
@@ -21,8 +21,9 @@ library(statnet)
 library(igraph)
 library(qgraph)
 
-image.dir	<- c("C:/Users/brh22/Dropbox/Apps/ShareLaTeX/Sabbatical Technical Note/Images/")
-load("DF.results.full.Rda")
+# image.dir	<- c("C:/Users/brh22/Dropbox/Apps/ShareLaTeX/Sabbatical Technical Note/Images/")
+image.dir	<- c("C:/Users/brh22/Documents/R Projects/SPS_Graphs_test/")
+# load("DF.results.full.Rda")
 df <- DF.results[order(DF.results$PRR.curr),] %>% 
   mutate(scenario=seq(1:nrow(DF.results))) %>%
   sort_rows_cols(.,margin=2,colorder=sort(colnames(.))) 
@@ -55,17 +56,16 @@ fin.names       <- c(paste0("F",seq(1:Fin.n)))
 #
 # Node Names
 #
-phys.units	<-	c("MW")
+phys.units	<-	c("mWh")
 Res.names 	<- matrix(c("Coal","NG"))
 Res.units	<- c(rep(phys.units,2))
 #Res.prices.units	<- c("MT","MMBTU")
-Res.prices.units	<- c("W","W")
+Res.prices.units	<- c("mWh","mWh")
 
 
 Fin.names	<- c("Res","Com") 
 Fin.units	<- c(rep(phys.units,2))
-# Fin.prices.units	<- c("kWh","kWh")
-Fin.prices.units	<- c("W","W")
+Fin.prices.units	<- c("mWh","mWh")
 
 Mfg.names	<- rep("",Mfg.n)
 for(i in 1:Mfg.n){ Mfg.names[i] <- paste("I",i,sep = "") }
@@ -86,50 +86,80 @@ curr.scale.display <- "Millions USD"
 
 ##############################################################################################################
 #
-# Create Physical and Monetary IO Matrices from DF.results
+# Create Physical and Monetary IO Network Diagrams from DF.results
 #
 ##############################################################################################################
 #
-# 1. Choose the scenarios that have the max PRR.curr, PRR.phys, F.phys
+# 1. For each price regime, choose the scenarios that have the 
+#                                           max PRR.curr (Value-added), 
+#                                           PRR.phys  (Net Energy Ratio), 
+#                                           F.phys,curr (Robustness)
 #
 #######################################################################################
-max.PRR.curr = df$scenario[[which.max(df$PRR.curr)]]
-max.PRR.phys = df$scenario[[which.max(df$PRR.phys)]]
-max.F.curr   = df$scenario[[which.max(df$F.curr)]]
-max.F.phys   = df$scenario[[which.max(df$F.phys)]]
-max.alpha.curr   = df$scenario[[which.max(df$alpha.curr)]]
-max.alpha.phys   = df$scenario[[which.max(df$alpha.phys)]]
-mid.scenario1 = nrow(df)/4
-mid.scenario2 = nrow(df)/2
-mid.scenario3 = round(nrow(df)/1.5,0)
+tally(group_by(df,Resources,Res.1.price,Res.2.price))
 
-scenarios.to.graph <- list(max.PRR.curr,max.PRR.phys,
-     max.F.phys,max.F.curr,
-     max.alpha.curr,max.alpha.phys,
-     mid.scenario1,mid.scenario2,mid.scenario3)
-names(scenarios.to.graph) <- list("max.PRR.curr","max.PRR.phys",
-                                  "max.F.phys","max.F.curr",
-                                  "max.alpha.curr","max.alpha.phys",
-                                  "mid.scenario1","mid.scenario2","mid.scenario3")
+#Get price regimes. 
+Coal_prices   <- levels(as.factor(df$Res.1.price))
+NG_prices     <- levels(as.factor(df$Res.2.price))
+NG.regimes    <- length(NG_prices) 
+Coal.regimes  <- length(Coal_prices)
+price.regimes <- NG.regimes * Coal.regimes
 
-txt <- sapply(names(scenarios.to.graph),
-              function(x) paste(x,paste(scenarios.to.graph[[x]],collapse=" ")))
+# Initialize vectors
+max.PRR.curr <- c(rep(0,price.regimes))
+max.PRR.phys <- c(rep(0,price.regimes))
+max.F.curr <- c(rep(0,price.regimes))
+max.F.phys <- c(rep(0,price.regimes))
 
-lapply(txt, cat, "\n", file=paste0(image.dir,"Flows_scen_key.txt"), append=TRUE)
+# The following command assumes only the price of NG changes:
+for(regime in 1:NG.regimes) {
+  df.temp <- filter(df,df$Res.2.price == NG_prices[regime])
+    max.PRR.curr[regime] = df.temp$scenario[[which.max(df.temp$PRR.curr)]]
+    max.PRR.phys[regime] = df.temp$scenario[[which.max(df.temp$PRR.phys)]]
+    max.F.curr[regime]   = df.temp$scenario[[which.max(df.temp$F.curr)]]
+    max.F.phys[regime]   = df.temp$scenario[[which.max(df.temp$F.phys)]]
+}
+
+scenarios.to.graph <- c(max.PRR.curr,max.PRR.phys,max.F.phys,max.F.curr)
+
+names(scenarios.to.graph) <- c(paste0("Max Value-Added (Price Regime ", 1:NG.regimes,")"),
+                               paste0("Max Net Energy Ratio (Price Regime ", 1:NG.regimes,")"),
+                               paste0("Max Biophysical Resilience (Price Regime ", 1:NG.regimes,")"),
+                               paste0("Max Economic Resilience (Price Regime ", 1:NG.regimes,")"))
+
+fnames <- c(paste0("max_va_", 1:NG.regimes),
+            paste0("max_ne_", 1:NG.regimes),
+            paste0("max_F_phys_", 1:NG.regimes),
+            paste0("max_F_curr_", 1:NG.regimes))
+
+paste0("Price Regimes \n") %>% 
+  write(file=paste0(image.dir,"Flows_scen_key.txt"),append=TRUE)
+
+paste("Price Regime",seq(1,price.regimes),"NG:", NG_prices, "Coal:", Coal_prices, 
+      sep= " ", collapse = "\n") %>% 
+  write(file=paste0(image.dir,"Flows_scen_key.txt"),append=TRUE)
+  
+paste(names(scenarios.to.graph), scenarios.to.graph, sep = ":\t", collapse = "\n") %>% 
+  write(file=paste0(image.dir,"Flows_scen_key.txt"),append=TRUE) 
 
 ######################################################################################
 # 2. Loop through the chosen scenarios and create graphs.pdf
 ######################################################################################
-for(scenario in scenarios.to.graph)  { 
+for(s in 1:length(scenarios.to.graph))  { 
+scenario <- scenarios.to.graph[s]
+scenario.name <- names(scenarios.to.graph)[s]
+filename <- fnames[s]
+
+va3 <- ifelse(is.nan(df$VA.3[[scenario]]),NA,round(df$VA.3[[scenario]],2))
+va4 <- ifelse(is.nan(df$VA.4[[scenario]]),NA,round(df$VA.4[[scenario]],2))
+va5 <- ifelse(is.nan(df$VA.5[[scenario]]),NA,round(df$VA.5[[scenario]],2))
+va6 <- ifelse(is.nan(df$VA.6[[scenario]]),NA,round(df$VA.6[[scenario]],2))
 
 Mfg.etas = c(round(df$Eta.3[[scenario]],2),
              round(df$Eta.4[[scenario]],2),
              round(df$Eta.5[[scenario]],2),
              round(df$Eta.6[[scenario]],2))
-VAs      = c(round(df$VA.3[[scenario]],2),
-             round(df$VA.4[[scenario]],2),
-             round(df$VA.5[[scenario]],2),
-             round(df$VA.6[[scenario]],2))
+VAs      = c(va3,va4,va5,va6)
 Res.prices	= c(round(df$Res.1.price[[scenario]],2),
                 round(df$Res.2.price[[scenario]],2))
 Fin.prices  = c(round(df$Fin.1.price[[scenario]],2),
@@ -184,10 +214,10 @@ num.fin.flows <- colSums(df$Flows.phys[[scenario]] != 0)[7:8] %>% sum()
 edge.label.position.vals <- c(rep(.5,num.res.flows),rep(.7,(num.fin.flows)))
 
 groups 	<- list(	"Input Units" =c(Res.nodes),
-                 "MW produced per MW"=c(Mfg.nodes),
+                 "Net Energy Ratio"=c(Mfg.nodes),
                  "Output Units"=c(Fin.nodes))
 groups.curr	<- list(	"Input Prices"=c(Res.nodes),
-                     "Value-Added per $"=c(Mfg.nodes),
+                     "Value-Added"=c(Mfg.nodes),
                      "Output Prices"=c(Fin.nodes))
 
 shapes <- c(rep("ellipse",Res.n),rep("rectangle",Mfg.n),rep("ellipse",Fin.n))
@@ -198,32 +228,9 @@ L <- matrix(c(
   2,2.5, 2,.5),
   ncol=2,byrow=TRUE)
 ############################################################################################
-# Graph Physical Flows network
-############################################################################################# 
-pdf(file=paste0(image.dir,"Flows_phys_",scenario,".pdf"))
-qgraph(df$Flows.phys[[scenario]],  
-       edge.labels=T,
-       edge.label.cex=1.25,edge.color="black",fade=F,
-       edge.label.position=edge.label.position.vals,
-       layout=L,
-       groups=groups,
-       borders=F,
-       labels=nodes.names,
-       nodeNames=nodes.names.phys.legend,
-       shape=shapes,
-       palette="colorblind",
-       title=paste0("Total Final Output ",df$TFO[[scenario]]," (MW)     ",
-       "Efficiency =",round(df$PRR.phys[[scenario]],2),
-       " (phys), ",round(df$PRR.curr[[scenario]],2), " (curr)", 
-       "\n a=",round(df$alpha.phys[[scenario]],2),
-       "\n F=",round(df$F.phys[[scenario]],2),
-       "\n (Flows in MW)") )
-dev.off()
-
-############################################################################################
 # Graph Currency Flows network
 ############################################################################################
-pdf(file=paste0(image.dir,"Flows_curr_",scenario,".pdf"))
+pdf(width=7, file=paste0(image.dir,paste0(filename,"_curr"),".pdf"))
 
 qgraph(df$Flows.curr[[scenario]],
         edge.labels=T,
@@ -236,13 +243,45 @@ qgraph(df$Flows.curr[[scenario]],
         nodeNames=nodes.names.curr.legend,
         shape=shapes,
         palette="colorblind",
-        title=paste0("GDP $",format(df$GDP.curr[[scenario]],width=7)," (million)               ",
-                    "Efficiency =",round(df$PRR.phys[[scenario]],2),
-                    " (phys), ",round(df$PRR.curr[[scenario]],2), " (curr)", 
-                    "\n a=",round(df$alpha.curr[[scenario]],2),
-                    "\n F=",round(df$F.curr[[scenario]],2),
-                    "\n (Flows in $millions)") )
+        mar=c(3,3,5,3),
+        title=paste0(scenario.name,
+                    # "\tGDP $",format(df$GDP.curr[[scenario]],width=7)," (million)",
+                     "\t (Flows in $millions)",
+                    "\n \t Net Energy Ratio = ",round(df$PRR.phys[[scenario]],2),
+                    "\n \t Value-Added = $",round(df$PRR.curr[[scenario]],2), 
+                    "\n \t F_econ =",round(df$F.curr[[scenario]],2),
+                    "\t (a=",round(df$alpha.curr[[scenario]],2),")",
+                    "\n \t F_phys =",round(df$F.phys[[scenario]],2),
+                    "\t (a=",round(df$alpha.phys[[scenario]],2),")"
+                    ) )
 dev.off()       
 
-}
+############################################################################################
+# Don't Graph Physical Flows network
+############################################################################################# 
+# pdf(file=paste0(image.dir,paste0(filename,"_phys"),".pdf"))
+# qgraph(df$Flows.phys[[scenario]],  
+#        edge.labels=T,
+#        edge.label.cex=1.25,edge.color="black",fade=F,
+#        edge.label.position=edge.label.position.vals,
+#        layout=L,
+#        groups=groups,
+#        borders=F,
+#        labels=nodes.names,
+#        nodeNames=nodes.names.phys.legend,
+#        shape=shapes,
+#        palette="colorblind",
+#        mar=c(3,3,5,3),
+#        title=paste0(scenario.name,
+#        # "\nTotal Final Output ",df$TFO[[scenario]]," (mWh)",
+#        " (Flows in mWh)",
+#        "\n \t Net Energy Ratio = ",round(df$PRR.phys[[scenario]],2),
+#        "\n \t Value-Added = $",round(df$PRR.curr[[scenario]],2),
+#        "\n \t F_econ =",round(df$F.curr[[scenario]],2),
+#        "\t (a=",round(df$alpha.curr[[scenario]],2),")",
+#        "\n \t F_phys =",round(df$F.phys[[scenario]],2),
+#        "\t (a=",round(df$alpha.phys[[scenario]],2),")"
+#        ) )
+# dev.off()
 
+}
